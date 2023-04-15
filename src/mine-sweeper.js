78ef6b2eacfa9228156610f71b64af37792569ff
KASSTUSS <kostya2003112015@gmail.com>
2023-04-15 22:52:02 +0300

    feat: complited mine-sweeper task

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,70 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  const rowsCount = matrix.length;
+  const colsCount = matrix[0].length;
+
+  const result = [];
+
+  // initialize result matrix with zeroes
+  for (let i = 0; i < rowsCount; i++) {
+    result.push(Array(colsCount).fill(0));
+  }
+
+  for (let row = 0; row < rowsCount; row++) {
+    for (let col = 0; col < colsCount; col++) {
+      // check top cell
+      if (row != 0) {
+        if (matrix[row - 1][col]) {
+          result[row][col]++;
+        }
+        if (col != colsCount - 1) {
+          if (matrix[row - 1][col + 1]) {
+            result[row][col]++;
+          }
+        }
+        if (col != 0) {
+          if (matrix[row - 1][col - 1]) {
+            result[row][col]++;
+          }
+        }
+      }
+      
+      // check bottom cell
+      if (row != rowsCount - 1) {
+        if (matrix[row + 1][col]) {
+          result[row][col]++;
+        }
+        if (col != colsCount - 1) {
+          if (matrix[row + 1][col + 1]) {
+            result[row][col]++;
+          }
+        }
+        if (col != 0) {
+          if (matrix[row + 1][col - 1]) {
+            result[row][col]++;
+          }
+        }
+      }
+      
+      // check right cell
+      if (col != colsCount - 1) {
+        if (matrix[row][col + 1]) {
+          result[row][col]++;
+        }
+      }
+      
+      // check left cell
+      if (col != 0) {
+        if (matrix[row][col - 1]) {
+          result[row][col]++;
+        }
+      }
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
